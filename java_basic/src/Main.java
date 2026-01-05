7504cd02af3dd511b4c1c18b546297ff15429aa6
hankmillersgithub <m18335784129@163.com>
2026-01-05 12:21:10 +0800

    2026.1.5 12:20 提交 数组

---
+++ java_basic/src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {33,5,22,44,55,33};
+        Scanner sc =new Scanner(System.in);
+        /*
+        查找元素
+        int i = sc.nextInt();
+        boolean flag = false;
+        for(int j=0;j<arr.length;j++){
+            if(i == arr[j]){
+                System.out.println(j);
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            System.out.println(-1);
+        }*/
+        /*查找最大值
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);*/
+    }
+}
